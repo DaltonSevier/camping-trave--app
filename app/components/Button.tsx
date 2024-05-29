ffd6820b0e5654c0b70cff6feeb48c8b709936d3
Dalton Sevier <daltonsevier@gmail.com>
2024-05-28 20:10:37 -0400

    implement full landing page

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,17 +2,18 @@ type ButtonProps  = {
     type: 'button' | 'submit';
     title: string;
     icon?: string;
-    variant: 'btn_dark_green' | 'btn_white_text' | 'btn_green';
+    variant: 'btn_dark_green' | 'btn_white_text' | 'btn_green' | 'btn_white' | 'btn_dark_green_outline';
+    full?: boolean
 }
 
 import Image from 'next/image'
 import React from 'react'
 
-const Button = ({type, title, icon, variant}:ButtonProps) => {
+const Button = ({type, title, icon, variant, full}:ButtonProps) => {
   return (
-    <button className={`flexCenter gap-3 rounded-full border ${variant}`} type={type}>
+    <button className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`} type={type}>
         {icon && <Image src={icon} alt={title} height={24} width={24}/>}
-        <label className='bold-16 whitespace-nowrap'>{title}</label>
+        <label className='bold-16 whitespace-nowrap cursor-pointer'>{title}</label>
     </button>
   )
 }
